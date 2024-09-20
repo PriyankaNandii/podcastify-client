e80611f62dac552b58f6fdac778073265b327216
masuma-riya <masumariya209@gmail.com>
2024-09-20 22:07:25 +0600

    fixed navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,15 @@
-<<<<<<< HEAD
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import './index.css'
-import router from "./Routes/Router";
-import { RouterProvider } from 'react-router-dom'
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
 import { RouterProvider } from "react-router-dom";
-import router from "./Routes/Routes";
 import AuthProviders from "./Providers/AuthProviders";
+import router from "./Routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProviders>
       <RouterProvider router={router} />
     </AuthProviders>
->>>>>>> 7bae41bab359b454004711dd8e3785f55cda1279
   </React.StrictMode>
 );
