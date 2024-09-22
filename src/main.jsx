e5e96fdf905c5ba40deda7b435a38ff271d36041
TinnyDatta <tinnydatta.prof@gmail.com>
2024-09-22 23:22:47 +0600

    added title

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import './index.css'
@@ -8,7 +7,6 @@ import { RouterProvider } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -21,6 +19,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <AuthProviders>
       <RouterProvider router={router} />
     </AuthProviders>
->>>>>>> 7bae41bab359b454004711dd8e3785f55cda1279
   </React.StrictMode>
 );
