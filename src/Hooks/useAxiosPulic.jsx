6fe4b30de21557b0c402e954c6809e2cf4f975dc
impactgalaxy <traderglow@gmail.com>
2024-10-06 17:34:02 +0600

    added music from database on music plyer

--- src/Hooks/useAxiosPulic.jsx
+++ src/Hooks/useAxiosPulic.jsx
@@ -2,11 +2,7 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
   baseURL: "http://localhost:5000",
-<<<<<<< HEAD
   // baseURL: "https://podcastify-server.vercel.app"
-=======
-  // baseURL: "https://podcastify-server.vercel.app",
->>>>>>> 47646b6f672f042d5da7ddf0075d227908a9afc1
 });
 const useAxiosPublic = () => {
   return axiosPublic;
--- src/components/dashboard-component/cards/MusicPlayer.jsx
+++ src/components/dashboard-component/cards/MusicPlayer.jsx
@@ -4,35 +4,116 @@ import { HiArrowPathRoundedSquare } from "react-icons/hi2";
 import { IoMdSkipBackward, IoMdSkipForward } from "react-icons/io";
 import { TbPlayerPause } from "react-icons/tb";
 import { TbPlayerPlayFilled } from "react-icons/tb";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import useAxiosPublic from "../../../Hooks/useAxiosPulic";
 
 export default function MusicPlayer() {
-  const [musicPlayOrPause, setMusicPlayOrPause] = useState(false);
+  const [trackList, setTrackList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axiosPublic.get("/podcast");
+        setTrackList(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+    }
+  }, [currentTrackIndex, isPlaying]);
+
+  const playNextTrack = () => {
+    setIsPlaying(false);
+    setCurrentTrackIndex((prevIndex) =>
+      prevIndex === trackList.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const playPreviousTrack = () => {
+    setIsPlaying(false);
+    setCurrentTrackIndex((prevIndex) =>
+      prevIndex === 0 ? trackList.length - 1 : prevIndex - 1
+    );
+  };
+
+  const playAudio = () => {
+    setIsPlaying(true);
+    audioRef.current.play();
+  };
+
+  const pauseAudio = () => {
+    setIsPlaying(false);
+    audioRef.current.pause();
+  };
+  const handlePlay = () => {
+    setIsPlaying(true);
+  };
+
+  const handlePause = () => {
+    setIsPlaying(false);
+  };
+
+  if (loading) {
+    return <h1 className="text-center text-sm">Please wait...</h1>;
+  }
   return (
     <div className="text-center space-y-8 p-4 rounded-xl bg-[#23232A] select-none">
       <h1 className="text-white text-lg">Now Playing</h1>
-      <img src={mike} alt="" className="w-full h-36 rounded-2xl mx-auto" />
+      <img
+        src={trackList[currentTrackIndex].coverImageUrl}
+        alt=""
+        className="w-full h-36 rounded-2xl mx-auto"
+      />
       <div className="font-bold text-center">
-        <h1 className="text-white">Eventually</h1>
-        <h1 className="text-[#616166]">By monir khan</h1>
+        <h2>Now: {trackList[currentTrackIndex].title}</h2>
+        <h2 className="text-[#616166]">
+          By: {trackList[currentTrackIndex].musician}
+        </h2>
       </div>
-      <div className="font-black flex items-center justify-evenly text-[12px]">
-        <h1 className="text-white">2:20</h1>
-        <div className="h-2 bg-[#616166] rounded-lg md:w-28 w-36 relative border">
-          <div className="h-2 bg-white rounded-lg w-20 absolute top-0 left-0"></div>
-        </div>
-        <h1 className="text-[#616166]">5:20</h1>
-      </div>
+      <audio
+        ref={audioRef}
+        src={trackList[currentTrackIndex].audioFileUrl}
+        onPlay={handlePlay}
+        onPause={handlePause}
+        controls
+      />
+
       <div className="flex items-center justify-between text-white *:text-xl *:cursor-pointer">
         <HiArrowPathRoundedSquare />
         <div className="flex items-center justify-center gap-3">
-          <IoMdSkipBackward />
+          <IoMdSkipBackward
+            onClick={playPreviousTrack}
+            className="hover:bg-slate-300 p-2 rounded-2xl text-4xl"
+          />
           <div
-            onClick={() => setMusicPlayOrPause(!musicPlayOrPause)}
+            onClick={() => {
+              if (isPlaying) {
+                pauseAudio();
+              } else {
+                playAudio();
+              }
+            }}
             className="border-2 p-2 rounded-2xl bg-white text-black">
-            {musicPlayOrPause ? <TbPlayerPause /> : <TbPlayerPlayFilled />}
+            {isPlaying ? <TbPlayerPause /> : <TbPlayerPlayFilled />}
           </div>
-          <IoMdSkipForward />
+          <IoMdSkipForward
+            onClick={playNextTrack}
+            className="hover:bg-slate-300 p-2 rounded-2xl text-4xl"
+          />
         </div>
         <TiArrowShuffle />
       </div>
