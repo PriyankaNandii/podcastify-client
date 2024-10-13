3d23a279f86fb9dcab738d00eefc14dbb8ad9efb
impactgalaxy <traderglow@gmail.com>
2024-10-13 14:22:49 +0600

    added chakra ui package

--- src/main.jsx
+++ src/main.jsx
@@ -6,21 +6,21 @@ import { RouterProvider } from "react-router-dom";
 import AuthProviders from "./Providers/AuthProviders";
 import router from "./Routes/Routes";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import {
-  QueryClient,
-  QueryClientProvider
-} from '@tanstack/react-query'
+import "react-toastify/dist/ReactToastify.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ChakraProvider } from "@chakra-ui/react";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <AuthProviders>
-      <RouterProvider router={router} />
-    </AuthProviders>
-    <ToastContainer />
-    </QueryClientProvider>
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProviders>
+          <RouterProvider router={router} />
+        </AuthProviders>
+        <ToastContainer />
+      </QueryClientProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
